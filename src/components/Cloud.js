bd3cb14eaac8b30537a802c120b01d8a8e1000b4
Yatish14 <yatishpandiri@gmail.com>
2023-05-15 17:20:28 +0530

    changes in contactform

--- src/components/Cloud.js
+++ src/components/Cloud.js
@@ -16,11 +16,17 @@ class Cloud extends React.Component {
       showModal: !prevState.showModal
     }));
   }
+  if(showModal)
+  {
+    console.log("");
+  }
+
 
   render() {
+    const rpaClass = this.state.showModal ? "rpa blur-background" : "rpa";
     return (
-      <div className='btnchoice'>
-      <div className='rpa'>
+      <div className="btnchoice">
+      <div className={rpaClass}>
         <div className='text'>
             <h2>Embrace Cloud</h2>
             <h3>With Cloud-First accelerate innovation and optimize performance</h3>
@@ -44,46 +50,82 @@ class Cloud extends React.Component {
             </div>
             <div className="modal-body">
               <form>
-                <label>
-                  First Name:<br />
-                  <input type="text" name="name" />
-                </label>
-                {/* <br /> */}
-                <label>
-                  Last Name:<br />
-                  <input type="text" name="name" />
-                </label>
-                {/* <br/> */}
-                <label>
-                  Bussiness Phone:<br />
-                  <input type='text' name="phone" />
-                </label>
-                {/* <br /> */}
-                <label>
-                  Work Email:<br />
-                  <input type='email' name="email" />
-                </label>
-                {/* <br /> */}
-                <label>
-                  Organization:<br />
-                  <input type='text' name="org" />
-                </label>
-                {/* <br /> */}
-                <label>
-                  Job Role:<br />
-                  <input type='text' name="role" />
-                </label>
-                {/* <br /> */}
-                <label>
-                  Country:<br />
-                  <input type='text' name="text" />
-                </label>
-                {/* <br /> */}
-                <label>
-                  Message:<br />
-                  <input type='text' name="message" />
-                </label>
-                {/* <br /> */}
+
+                <div className='formcont'>
+
+                    <div className='formcontent'>
+                      <div className='forminp'>
+                        <label>
+                          First Name*<br />
+                          <input type="text" name="name" />
+                        </label>
+                      </div>
+                      <div className='forminp'>
+                        {/* <br /> */}
+                        <label>
+                          Last Name*<br />
+                          <input type="text" name="name" />
+                        </label>
+                      </div>
+                    </div>
+
+
+                    <div className='formcontent'>
+                      <div className='forminp'>
+                        <label>
+                        Bussiness Phone<br />
+                        <input type='text' name="phone" />
+                        </label>
+                      </div>
+                      <div className='forminp'>
+                        {/* <br /> */}
+                        <label>
+                        Work Email<br />
+                        <input type='email' name="email" />
+                      </label>
+                      </div>
+                    </div>
+
+
+                    <div className='formcontent'>  
+                      <div className='forminp'>
+                        <label>
+                        Organization<br />
+                        <input type='text' name="org" />
+                      </label> 
+                      </div>
+                      <div className='forminp'>
+                        {/* <br /> */}
+                        <label>
+                        Job Role<br />
+                        <input type='text' name="role" />
+                      </label>
+                      </div>
+                    </div>
+
+
+                    <div>
+                      <label>
+                        Country*<br />
+                        <select id="country-dropdown">
+                        <option value="">--Select a country--</option>
+                        <option value="AF">Afghanistan</option>
+                        <option value="AL">Albania</option>
+                        <option value="DZ">Algeria</option>
+                        <option value="IN">India</option>
+                        <option value="ZW">Zimbabwe</option>
+                        </select>
+                      </label>
+                    </div>
+
+                    <div className='forminput'>
+                      <label>
+                        Your Message<br />
+                        <input type='text' name="message" />
+                      </label>
+                    </div>
+
+                </div>
               </form>
             </div>
             <div className="modal-footer">
